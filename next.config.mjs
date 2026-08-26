d019b189d0f26d28b0391581e832c733fb79c296
erxes <dev@erxes.io>
2026-08-26 16:20:02 +0800

    chore: initial site build

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig = {
   images: {
     unoptimized: true,
+    remotePatterns: [
+      { protocol: "https", hostname: "api.qrserver.com", pathname: "/**" },
+    ],
   },
   env: {
     NEXT_PUBLIC_ERXES_ENDPOINT: "https://enkhee.next.erxes.io/gateway/graphql",
